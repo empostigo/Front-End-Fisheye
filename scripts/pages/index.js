a7bb8cc28e4b2d334db748c1cbf0bebd2637f4b3
empostigo <emmanuel.postigo@tutanota.com>
2023-05-19 10:33:07 +0200

    Renames class photgrapher_section to photographers

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -10,7 +10,7 @@ async function getPhotographers() {
 }
 
 async function displayData(photographers) {
-  const photographersSection = document.querySelector(".photographer_section")
+  const photographersSection = document.querySelector(".photographers")
 
   photographers.forEach((photographer) => {
     const photographerModel = new PhotographerFactory(photographer, "home")
