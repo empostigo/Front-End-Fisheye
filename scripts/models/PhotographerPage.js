e02dcc6bdf618287c80612d74a083dda7b9cb68c
empostigo <emmanuel.postigo@tutanota.com>
2023-05-20 21:01:39 +0200

    Add div for tabindex

--- scripts/models/PhotographerPage.js
+++ scripts/models/PhotographerPage.js
@@ -9,18 +9,17 @@ export class PhotographerPage extends BasePage {
     // set class names , BEM notation
     const parentClass = "photographer-header"
     const descriptionClass = `${parentClass}__description`
+    const infosClass = `${parentClass}__infos`
     const h1Class = `${parentClass}__heading`
     const locationClass = `${parentClass}__location`
     const pTagLineClass = `${parentClass}__tagline`
     const imageClass = `${parentClass}__portrait`
 
     // Name, city, country and tagline
-    const description = document.createElement("div")
-    description.className = descriptionClass
-
     const h1 = document.createElement("h1")
     h1.className = h1Class
     h1.textContent = this.name
+    h1.tabIndex = 0
 
     const location = document.createElement("p")
     location.className = locationClass
@@ -30,14 +29,22 @@ export class PhotographerPage extends BasePage {
     pTagLine.className = pTagLineClass
     pTagLine.textContent = this.tagline
 
+    const infos = document.createElement("div")
+    infos.className = infosClass
+    infos.tabIndex = 0
+    infos.appendChild(location)
+    infos.appendChild(pTagLine)
+
+    const description = document.createElement("div")
+    description.className = descriptionClass
     description.appendChild(h1)
-    description.appendChild(location)
-    description.appendChild(pTagLine)
+    description.appendChild(infos)
 
     // displaying portrait
     const image = document.createElement("img")
     image.className = imageClass
     image.setAttribute("src", this.picture)
+    image.tabIndex = 0
 
     return { description, image }
   }
