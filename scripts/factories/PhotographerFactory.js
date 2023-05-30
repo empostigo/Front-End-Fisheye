8d48dbd4c08d93bccce47fd252200818f1b90e6a
empostigo <emmanuel.postigo@tutanota.com>
2023-05-30 18:22:03 +0200

    include the case where the site starts, i.e the url does not include index.html

--- scripts/factories/PhotographerFactory.js
+++ scripts/factories/PhotographerFactory.js
@@ -5,7 +5,8 @@ export class PhotographerFactory {
   constructor(data, otherData = "") {
     this.pathname = new URL(document.location).pathname.replace("/", "")
     // HomePage (index.js) photographers type
-    if (this.pathname === "index.html") return new HomePage(data)
+    if (!this.pathname || this.pathname === "index.html")
+      return new HomePage(data)
     // PhotographerPage (photographer.js) photographer type
     else if (this.pathname === "photographer.html")
       return new PhotographerPage(data, otherData)
