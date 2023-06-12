da5db458808992d1725d01c5f70ab737a6b9adb2
empostigo <emmanuel.postigo@tutanota.com>
2023-06-12 13:21:28 +0200

    Implement listbox click fonctionnality

--- scripts/utils/Sorting.js
+++ scripts/utils/Sorting.js
@@ -1,6 +1,7 @@
 export class Sorting {
   constructor(sortingElement) {
     this.sortingElement = sortingElement
+    this.categories = document.querySelector(".sorting__categories")
     this.sortingOptions = Array.from(
       document.querySelectorAll(".sorting__option")
     )
@@ -9,57 +10,69 @@ export class Sorting {
     this.down = document.getElementById("down")
   }
 
-  openDropDown() {
-    this.up.classList.add("sorting__icon--hidden")
-    this.down.classList.remove("sorting__icon--hidden")
+  // Display arrow up icon listbox
+  openedDropDown() {
+    this.up.classList.remove("sorting__icon--hidden")
+    this.down.classList.add("sorting__icon--hidden")
     this.sortingElement.setAttribute("aria-expanded", true)
   }
 
-  closeDropDown() {
-    this.up.classList.remove("sorting__icon--hidden")
-    this.down.classList.add("sorting__icon--hidden")
+  // Display arrow down icon listbox
+  closedDropDown() {
+    this.up.classList.add("sorting__icon--hidden")
+    this.down.classList.remove("sorting__icon--hidden")
     this.sortingElement.setAttribute("aria-expanded", false)
   }
 
   displayOption(index) {
-    this.closeDropDown()
     this.sortingOptions.forEach((element) => {
       element.classList.add("sorting__option--hidden")
-      element.classList.remove("sorting__option--top-border")
+      element.classList.remove("sorting__option--no-border")
       element.setAttribute("aria-selected", false)
     })
-    this.sortingOptions[index].classList.remove("sorting__option--hidden")
-    this.sortingOptions[index].classList.add("sorting__option--top-border")
-    this.sortingOptions[index].setAttribute("aria-selected", true)
+    this.closedDropDown()
+    const displayedOption = this.sortingOptions[index]
+    displayedOption.classList.add("sorting__option--all-border")
+    displayedOption.classList.remove("sorting__option--hidden")
+    displayedOption.setAttribute("aria-selected", true)
+
+    this.sortingElement.style.zIndex = 2
   }
 
   displayAllOptions() {
-    this.openDropDown()
+    this.openedDropDown()
     this.sortingOptions.forEach((element) => {
-      element.classList.remove("sorting__option--hidden")
-      element.classList.remove("sorting__option--top-border")
+      element.className = "sorting__option"
     })
     this.sortingOptions[0].classList.add("sorting__option--top-border")
+    this.sortingOptions[0].classList.add("sorting__option--no-border")
+
+    this.sortingOptions[2].classList.add("sorting__option--radius-border")
+    this.sortingElement.style.zIndex = 0
   }
 
   static initElements(sortingObject) {
     sortingObject.displayOption(0)
+    sortingObject.categories.classList.remove("sorting__categories--hidden")
 
     let flag = 0
     sortingObject.sortingElement.addEventListener("click", () => {
-      if (flag++ % 2 === 0) {
-        sortingObject.displayAllOptions()
-        sortingObject.openDropDown()
-      } else sortingObject.closeDropDown()
+      if (flag++ % 2 === 0) sortingObject.displayAllOptions()
+      else
+        sortingObject.displayOption(
+          sortingObject.sortingElement.dataset.indexNumber % 10
+        )
     })
 
     sortingObject.sortingOptions.forEach((element) =>
       element.addEventListener("click", () => {
         sortingObject.displayOption(element.id % 10)
         sortingObject.sortingElement.dataset.indexNumber = element.id
+        flag++
       })
     )
 
+    /*
     let index = 0
     sortingObject.sortingElement.addEventListener("keydown", (event) => {
       if (event.key === "Enter") event.preventDefault()
@@ -83,5 +96,6 @@ export class Sorting {
         if (index < 0) index = 0
       }
     })
+  */
   }
 }
