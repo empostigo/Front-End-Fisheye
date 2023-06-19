e4e29c997033f848f488f3e2e33c14ddcfb54710
empostigo <emmanuel.postigo@tutanota.com>
2023-06-19 15:53:28 +0200

    Use arrow function in FetchData

--- scripts/Api/FetchData.js
+++ scripts/Api/FetchData.js
@@ -7,16 +7,11 @@ export default class FetchData {
     this.header.append("Cache-Control", this.maxAge)
   }
 
-  async get() {
-    return fetch(this.url, { headers: this.header })
+  getData = () =>
+    fetch(this.url, { headers: this.header })
       .then((response) => response.json())
       .then((data) => data[this.data]) // [this.data]: "photographers" or "media"
       .catch((err) =>
         console.error("Erreur lors de la lecture des données", err)
       )
-  }
-
-  async getData() {
-    return this.get()
-  }
 }
